905a520ca00f7c78629a9a5d482535512c49aa26
Aijia Wang <ajw7427@gmail.com>
2022-08-24 22:29:36 -0500

    Add an API router

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -13,7 +16,9 @@ router.get("/api/csrf/restore", (req, res) => {
   });
 });
 
-
+// {
+//   "XSRF-Token": "iNS69xdQ-YcJesyqwm8zyGuN5Q9Q57Cig45o"
+//   }
 
 
 
