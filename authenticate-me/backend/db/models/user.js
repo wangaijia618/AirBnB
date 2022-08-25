924e4452a8861c2dd971aae8b32104137a43d871
Aijia Wang <ajw7427@gmail.com>
2022-08-25 17:57:33 -0500

    Add User model authentication Sequelize scopes and methods

--- authenticate-me/backend/db/models/user.js
+++ authenticate-me/backend/db/models/user.js
@@ -1,5 +1,6 @@
 'use strict';
 const { Model, Validator } = require('sequelize');
+const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -7,10 +8,42 @@ module.exports = (sequelize, DataTypes) => {
       const { id, username, email } = this; // context will be the User instance
       return { id, username, email };
     }
+    validatePassword(password) {
+      return bcrypt.compareSync(password, this.hashedPassword.toString());
+    }
+
+    static getCurrentUserById(id) {
+      return User.scope("currentUser").findByPk(id);
+    }
     static associate(models) {
       // define association here
+ };
+
+    static async login({ credential, password }) {
+      const { Op } = require('sequelize');
+      const user = await User.scope('loginUser').findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential
+          }
+        }
+      });
+      if (user && user.validatePassword(password)) {
+        return await User.scope('currentUser').findByPk(user.id);
+      }
     }
-  };
+    static async signup({ username, email, password }) {
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({
+        username,
+        email,
+        hashedPassword
+      });
+      return await User.scope('currentUser').findByPk(user.id);
+    }
+  }
+
 
   User.init(
     {
@@ -61,35 +94,3 @@ module.exports = (sequelize, DataTypes) => {
   );
   return User;
 };
-
-validatePassword(password) {
-  return bcrypt.compareSync(password, this.hashedPassword.toString());
-}
-const bcrypt = require('bcryptjs');
-static getCurrentUserById(id) {
-  return User.scope("currentUser").findByPk(id);
-}
-
-static async login({ credential, password }) {
-  const { Op } = require('sequelize');
-  const user = await User.scope('loginUser').findOne({
-    where: {
-      [Op.or]: {
-        username: credential,
-        email: credential
-      }
-    }
-  });
-  if (user && user.validatePassword(password)) {
-    return await User.scope('currentUser').findByPk(user.id);
-  }
-}
-static async signup({ username, email, password }) {
-  const hashedPassword = bcrypt.hashSync(password);
-  const user = await User.create({
-    username,
-    email,
-    hashedPassword
-  });
-  return await User.scope('currentUser').findByPk(user.id);
-}
