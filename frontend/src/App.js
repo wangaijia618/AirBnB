16b45eb5b8131add9b3b639fd82588ffc868938e
Aijia Wang <ajw7427@gmail.com>
2022-09-16 15:20:38 -0500

    bonusphase1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
+// import LoginFormPage from "./components/LoginFormModal/LoginForm";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation"
@@ -29,9 +29,6 @@ return (
     <Navigation isLoaded={isLoaded} />
     {isLoaded && (
       <Switch>
-        <Route path="/login">
-          <LoginFormPage />
-        </Route>
         <Route path="/signup">
           <SignupFormPage />
         </Route>
